0948518a5467404807d64477df701b38b14a1202
alfito Dangkuhono <127445561+alfitoDangkuhono@users.noreply.github.com>
2025-10-01 22:07:57 +0700

    fix update

--- time.go
+++ time.go
@@ -9,9 +9,5 @@ func NowFormatted() string {
 	return time.Now().Format(time.RFC1123)
 }
 func NowDateTimeWithLooping(format string) string {
-	// for {
-		
-	// 	time.Sleep(1 * time.Second)
-	// }
 	return "Time Right Now Start : " + time.Now().Format(format)
 }
